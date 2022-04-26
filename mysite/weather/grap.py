8183ce9e2ac9bc5b04fc3b7efe65c561b5b72bb3
Evgeniy <men6586@yandex.ru>
2022-04-26 17:02:57 +0300

    matplotlib, show axis, add db waethers, city and other

--- mysite/weather/grap.py
+++ mysite/weather/grap.py
@@ -2,16 +2,27 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 from datetime import  datetime
-from .models import Weather
+from .models import Weather, City
 def asd():
     today = datetime.today()
     month = today.month
     dict_count_day = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}
     count_day = dict_count_day[month]
     now_day = today.day
-    weather_data = Weather.objects.filter(data='yandex')
-    print(weather_data.values('temperature')[0]['temperature'])
-    plt.axis([1, now_day+1,  -60, 60])
+    cities = City.objects.all()
+    result_wtr  = []
+    name_city = []
+    for city in cities:
+        weathers = Weather.objects.filter(id_city=city.id)
+        name_city.append(city.name)
+        spisok = []
+        for weather in weathers:
+            spisok.append(float(weather.temperature))
+        result = {city.id : spisok}
+        result_wtr.append(result)
+    msc = result_wtr[0][1]
+    print(msc)
+    plt.axis([1, now_day+1,  min(msc), max(msc)])
     plt.title(count_day)
-    #plt.plot(list(range(1, now_day)),), linewidth=2.0)
+    plt.plot(msc)
     plt.show()
