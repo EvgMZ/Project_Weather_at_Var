0562f445eb61785f519e38280d3ca5394b05b441
Evgeniy <men6586@yandex.ru>
2022-05-05 18:38:52 +0300

    dumpdata, create matploit, media

--- mysite/weather/grap.py
+++ mysite/weather/grap.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from datetime import  datetime
 from .models import Weather, City
+BASE_URL = './media/weather'
 def asd():
     today = datetime.today()
     month = today.month
@@ -11,31 +12,21 @@ def asd():
     count_day = dict_count_day[month]
     now_day = today.day
     cities = City.objects.all()
-    result_wtr  = []
-    name_city = []
-    spisok_data = ['yandex', 'wttr', 'gismeteto']
+    spisok_data = ['yandex', 'wttr', 'gismeteo']
+    dict_data = {'yandex': [], 'wttr': [], 'gismeteo': []}
+    plt.ioff()
+    cnt_day = [i for i in range(1, 5)]
     for city in cities:
         weathers = Weather.objects.filter(id_city=city.id)
-        spisok = []
         for data in spisok_data:
-            weathers = weathers.filter(data=data)
-            for weather in weathers:
+            spisok = []
+            weathers_fil = weathers.filter(data=data)
+            for weather in weathers_fil:
                 spisok.append(float(weather.temperature))
-            print(f'{city.name} {data} {spisok}')
-            plt.axis([1, now_day+1,  min(spisok)-2, max(spisok) +2])
-            plt.title(f'{city.name} {count_day}')
-            plt.plot(spisok)
-            plt.savefig(f'{city.name}_{count_day}.png')
+            dict_data[data] = spisok
+        plt.axis([1, now_day+1,  min(spisok)-2, max(spisok) +2])
+        plt.title(f'{city.name} {count_day}')
+        plt.plot(cnt_day, dict_data['yandex'],  'r.', cnt_day, dict_data['wttr'],'b.' , cnt_day, dict_data['gismeteo'], 'g.', linestyle='solid')
+        lgnd = plt.legend(['yandex', 'wttr', 'gismeteo'], loc='best', shadow=True)
+        plt.savefig(f'{BASE_URL}/{city.name}_{count_day}.png')
         plt.close()   
-        result = {city.id : spisok}
-        result_wtr.append(result)
-    result_wtr[0][1]
-    k = 1
-    '''for i in range(len(result_wtr)):
-        msc = result_wtr[i][k]
-        plt.axis([1, now_day+1,  min(msc), max(msc)])
-        plt.title(f'{name_city[i]} {count_day}')
-        plt.plot(msc)
-        plt.savefig(f'{name_city[i]}_{count_day}.png')
-        #plt.close()
-        k += 1'''
--- mysite/weather/views.py
+++ mysite/weather/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import requests
+import os
 from . import get_weather, grap
 
 def index(request):
     #get_weather.main()
-    grap.asd()
-    return HttpResponse("asd")
+    #grap.asd()
+    print(os.getcwd())
+    return render(request, 'index.html', {'url_image': 'D:\python_project\Project_Weather_at_Var\mysite\weather\media\Moscow_May.png'})
